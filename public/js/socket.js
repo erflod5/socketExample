2cf034d9cea9ab0563114218293b8dea4d3708fc
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-04-15 13:06:10 -0600

    package.json

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,5 @@
 //var sock =new WebSocket("ws://localhost:5001");
-var sock = new WebSocket("ws://192.168.1.187:3001"); //replace this address with the one the node.js server prints out. keep port 3000
+var sock = new WebSocket("ws://13.59.203.226:3001"); //replace this address with the one the node.js server prints out. keep port 3000
 var display = document.getElementById("display");
 sock.onopen = function(event) {
   alert("Socket connected succesfully!!!");
